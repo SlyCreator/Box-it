ee4bd806b8d0fb05873c5c97ed534a251a73b3aa
slycreator <chinonso1amaechi@gmial.com>
2020-05-19 11:53:00 +0100

    order migration configured

--- database/migrations/2020_05_19_101732_create_service_types_table.php
+++ database/migrations/2020_05_19_101732_create_service_types_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateOrdersTable extends Migration
+class CreateServiceTypesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,11 @@ class CreateOrdersTable extends Migration
      */
     public function up()
     {
-        Schema::create('orders', function (Blueprint $table) {
+        Schema::create('service_types', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->string('amount');
             $table->timestamps();
         });
     }
@@ -26,6 +29,6 @@ class CreateOrdersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('orders');
+        Schema::dropIfExists('service_types');
     }
 }
---
+++ database/migrations/2020_05_20_063539_create_orders_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('weight_in_kg')->default('0');
+            $table->string('height_in_m')->default('0');
+            $table->string('length_in_m')->default('0');
+
+            $table->unsignedBigInteger('service_type_id');
+            $table->foreign('service_type_id')->references('id')
+                ->on('service_types')->onDelete('cascade');
+
+            $table->unsignedBigInteger('shipping_id');
+            $table->foreign('shipping_id')->references('id')
+                ->on('shippings')->onDelete('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
