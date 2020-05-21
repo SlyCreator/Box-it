e8d82b336a6968213ebf1b75703647d3ae3ace2a
slycreator <chinonso1amaechi@gmial.com>
2020-05-21 12:56:50 +0100

    relationship set for order to shipping

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -7,8 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    protected $fillable = ['name','email','sent_from','weight_in_kg',
-        'height_in_m','length_in_m','service_type_id','user_id'];
+    protected $fillable = ['name','email','sent_from',
+                'weight_in_kg', 'height_in_m','length_in_m',
+                'service_type_id','user_id'];
+
     public function shipping()
     {
         return $this->hasOne(Shipping::class);
